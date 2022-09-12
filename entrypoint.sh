8a2c7f6af6a0755f8abdb347975103cab6760c0a
Martijn Riemers <martijn@riemers.com>
2022-09-13 01:28:28 +0200

    Better error reporting

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,11 @@
 set -e
 
 error() {
-  echo "::error::$1"
-  exit 1
+  echo -n "::error"
+  if [[ -n "$2" && -n "$3" && -n "$4" ]]; then
+    echo -n " file=$2,line=$3,col=$4"
+  fi
+  echo "::$1"
 }
 
 warn() {
@@ -27,6 +30,7 @@ include="${3}"
 
 if [[ -z "$version" ]]; then
   error "Input 'version' is missing."
+  exit 1
 fi
 
 if [[ "$version" == "4" ]]; then
@@ -35,10 +39,12 @@ elif [[ "$version" == "5" ]]; then
   executable="/metaeditor64.exe"
 else
   error "Only MetaEditor versions 4 and 5 are supported."
+  exit 1
 fi
 
 if [[ -z "$files" ]]; then
   error "Input 'files' is missing."
+  exit 1
 fi
 
 readarray -t files <<< "$files"
@@ -64,21 +70,27 @@ for f in "${expanded_files[@]}"; do
   wine "$executable" "${args[@]}" "/compile:$f" &> /dev/null || true
   if [[ ! -f $log ]]; then
     error "[$f] Log file does not exist."
+    exit 1
   fi
   content=$(iconv -f utf-16le -t ascii -c "$log" | sed -r '/^[[:space:]]+$/d')
   debug "$content"
   while IFS='=' read -r message _file row column; do
     warn "$message" "$_file" "$row" "$column"
   done < <(echo "$content" | sed -rn 's/([[:alnum:]_/-\.]+)\(([[:digit:]]+),([[:digit:]]+)\) : warning [[:digit:]]+: (.*)$/\4=\1=\2=\3/p')
+  while IFS='=' read -r message _file row column; do
+    error "$message" "$_file" "$row" "$column"
+  done < <(echo "$content" | sed -rn 's/([[:alnum:]_/-\.]+)\(([[:digit:]]+),([[:digit:]]+)\) : error [[:digit:]]+: (.*)$/\4=\1=\2=\3/p')
   IFS=' ' read -r errors warnings < <(echo "$content" | sed -rn 's/^[Rr]esult:? ([[:digit:]]+) errors, ([[:digit:]]+) warnings.*$/\1 \2/p')
   if [[ "$errors" -gt 0 ]]; then
     error "[$f] Compilation failed, $errors errors and $warnings warnings."
+    exit 1
   else
     if [[ -f "$output" ]]; then
       echo "[$f] Compilation succeeded, $errors errors and $warnings warnings."
       echo "[$f] Binary available at $output."
     else
       error "[$f] Compilation succeeded but file is not available."
+      exit 1
     fi
   fi
   rm -rf "$log"
